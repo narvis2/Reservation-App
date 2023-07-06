33615832a51df92c18532ce3e686a7ccf434ee17
narvis2 <narvis2@douzone.com>
2023-07-06 14:57:26 +0900

    🌸 각각 Process 에 맞는 View 생성 🌸
    - 각각의 Process 에 맞는 View 생성

---
+++ lib/presentation/views/reservation/view/reservation_fifth_process_view.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class ReservationFifthProcessView extends StatefulWidget {
+  const ReservationFifthProcessView({Key? key}) : super(key: key);
+
+  @override
+  State<ReservationFifthProcessView> createState() => _ReservationFifthProcessViewState();
+}
+
+class _ReservationFifthProcessViewState extends State<ReservationFifthProcessView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("예약 완료"),);
+  }
+}
---
+++ lib/presentation/views/reservation/view/reservation_fourth_process_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReservationFourthProcessView extends StatefulWidget {
+  const ReservationFourthProcessView({Key? key}) : super(key: key);
+
+  @override
+  State<ReservationFourthProcessView> createState() => _ReservationFourthProcessViewState();
+}
+
+class _ReservationFourthProcessViewState extends State<ReservationFourthProcessView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("좌석 선택"),);
+  }
+}
