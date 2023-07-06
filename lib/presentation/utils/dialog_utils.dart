efead89b1b2a5bbecdb428558ef0555c3f96c4ff
narvis2 <narvis2@douzone.com>
2023-07-06 14:18:43 +0900

    🌸 BaseDialog 추가 🌸
    - 싱글톤으로 어디서든 사용할 수 있는 BaseDialog 추가

--- lib/presentation/utils/dialog_utils.dart
+++ lib/presentation/utils/dialog_utils.dart
@@ -93,4 +93,125 @@ class DialogUtils {
       height: 300,
     );
   }
+
+  static showBasicDialog({
+    required BuildContext context,
+    required String title,
+    required String message,
+    bool enableCancelBtn = false,
+    bool enableAutoCancel = true,
+  }) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 60),
+            decoration: BoxDecoration(
+              color: ColorsConstants.background,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            height: 200,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: Text(
+                          title,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: ColorsConstants.divider,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Divider(
+                        height: 1,
+                        thickness: 1,
+                        indent: 15,
+                        endIndent: 15,
+                      ),
+                      Expanded(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.all(15),
+                          alignment: Alignment.center,
+                          child: Text(
+                            message,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: ColorsConstants.divider,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      if (enableCancelBtn)
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                              ),
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                ColorsConstants.primary,
+                              ),
+                            ),
+                            child: Text("취소"),
+                          ),
+                        ),
+                      SizedBox(width: enableCancelBtn ? 5 : 0),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                            ),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              ColorsConstants.inProgressColor,
+                            ),
+                          ),
+                          child: Text("확인"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      barrierDismissible: enableAutoCancel,
+    );
+  }
 }
