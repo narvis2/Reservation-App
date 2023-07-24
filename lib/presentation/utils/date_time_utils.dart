749994fca19cfee11c7ae4f92919b50f0226e98b
narvis2 <narvis2@douzone.com>
2023-07-24 16:14:42 +0900

    🌸 Process 1️⃣ - 예약 시간, 예약 날짜 수정 (당일 예약 대응) 🌸
    - 현재 시간이 각 PartTime 보다 이후일 경우 대응
    - 예약 날짜 👉 가장 늦은 예약인 PartTime.partC 를 기준으로 현재 시간이 PartTime.partC 시간보다 2시간 이상 빠른 경우 달력을 열때 초기값이 내일 날짜로 표시되고 오늘 날짜를 선택할 수 없게 수정
    - 예약 시간 👉 당일 예약 시 현재 시간이 각 PartTime 보다 2 시간 빠른 것에 해당하는 PartTime 의 경우 Visible GONE 처리하여 선택할 수 없도록 수정

--- lib/presentation/utils/date_time_utils.dart
+++ lib/presentation/utils/date_time_utils.dart
@@ -1,4 +1,5 @@
 import 'package:intl/intl.dart';
+import 'package:reservation_app/domain/model/reservation/enum/part_time.dart';
 
 class DateTimeUtils {
   static List<DateTime> disablePastTime() {
@@ -18,7 +19,8 @@ class DateTimeUtils {
     return listDateDisabled;
   }
 
-  static String dateTimeToString({required String pattern, required DateTime date}) {
+  static String dateTimeToString(
+      {required String pattern, required DateTime date}) {
     return DateFormat(pattern).format(date);
   }
 
@@ -31,5 +33,32 @@ class DateTimeUtils {
     int minutes = durationInSeconds ~/ 60;
     int seconds = durationInSeconds % 60;
     return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+  }
+
+  static DateTime dateTimeWithPartTime(DateTime date, PartTime partTime) {
+    final partTimeToString = partTime == PartTime.partA
+        ? '13:00'
+        : partTime == PartTime.partB
+            ? '17:50'
+            : partTime == PartTime.partC
+                ? '20:00'
+                : '00:00';
+
+    DateFormat formatter = DateFormat('yyyy-MM-dd\'T\'$partTimeToString');
+    return DateTime.parse(formatter.format(date));
+  }
+
+  /// 📌 주어진 시간이 현재 시간보다 이전인 경우 false
+  /// - 주어진 시간이 현재 시간보다 미래이고 그 차이가 1 보다 크면 true
+  /// - 즉, 선택한 시간이 현재 시간보다 2시간 이상 차이가 날 경우 true
+  static bool isDateWithinOneHour(DateTime date) {
+    final now = DateTime.now();
+
+    if (date.isAfter(now)) {
+      final difference = date.difference(now);
+      return difference.inHours > 1;
+    } else {
+      return false;
+    }
   }
 }
