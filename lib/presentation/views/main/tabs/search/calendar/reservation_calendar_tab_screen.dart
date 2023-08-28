d20bfee7ee63d82236e744136d02518a64a8466f
narvis2 <narvis2@douzone.com>
2023-08-28 16:55:18 +0900

    🦋 Table_Calendar 라이브러리를 활용한 예약 달력 기능 구현 🦋
    ## 🌸 예약 달력 기능 구현
    -
    
    ### 🌹 pubspec.lock
    - [table_calendar](https://github.com/aleksanderwozniak/table_calendar) 👉 달력 구현을 위해 추가
    
    ``` yaml
    table_calendar: ^3.0.9
    ```

--- lib/presentation/views/main/tabs/search/calendar/reservation_calendar_tab_screen.dart
+++ lib/presentation/views/main/tabs/search/calendar/reservation_calendar_tab_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class ReservationCalendarTabScreen extends StatefulWidget {
   const ReservationCalendarTabScreen({Key? key}) : super(key: key);
@@ -11,8 +12,11 @@ class ReservationCalendarTabScreen extends StatefulWidget {
 class _ReservationCalendarTabScreenState extends State<ReservationCalendarTabScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("달력"),
+    return TableCalendar(
+      locale: 'ko_KR',
+      firstDay: DateTime(DateTime.now().year),
+      lastDay: DateTime(DateTime.now().year + 1),
+      focusedDay: DateTime.now(),
     );
   }
 }
